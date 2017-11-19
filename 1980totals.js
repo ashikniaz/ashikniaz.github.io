bda9bb3d57cf351f7c265cd25c5daaa02f6cc760
JManos07 <32912727+JManos07@users.noreply.github.com>
2017-11-18 20:55:33 -0500

    Add files via upload

---
+++ 1980totals.js
@@ -0,0 +1,707 @@
+var countyTotals = {"type":"FeatureCollection","features":﻿[
+ {
+   "County": "Adams",
+   "White": 24121,
+   "Black": 25,
+   "American Indian and Alaska Native": 27,
+   "Asian and Pacific Islander": 18,
+   "Hispanic": 137
+ },
+ {
+   "County": "Allen",
+   "White": 99837,
+   "Black": 10785,
+   "American Indian and Alaska Native": 174,
+   "Asian and Pacific Islander": 367,
+   "Hispanic": 1040
+ },
+ {
+   "County": "Ashland",
+   "White": 45242,
+   "Black": 445,
+   "American Indian and Alaska Native": 31,
+   "Asian and Pacific Islander": 192,
+   "Hispanic": 230
+ },
+ {
+   "County": "Ashtabula",
+   "White": 99680,
+   "Black": 2904,
+   "American Indian and Alaska Native": 192,
+   "Asian and Pacific Islander": 316,
+   "Hispanic": 1093
+ },
+ {
+   "County": "Athens",
+   "White": 53339,
+   "Black": 1620,
+   "American Indian and Alaska Native": 118,
+   "Asian and Pacific Islander": 670,
+   "Hispanic": 528
+ },
+ {
+   "County": "Auglaize",
+   "White": 42311,
+   "Black": 34,
+   "American Indian and Alaska Native": 27,
+   "Asian and Pacific Islander": 78,
+   "Hispanic": 104
+ },
+ {
+   "County": "Belmont",
+   "White": 80328,
+   "Black": 1631,
+   "American Indian and Alaska Native": 39,
+   "Asian and Pacific Islander": 222,
+   "Hispanic": 327
+ },
+ {
+   "County": "Brown",
+   "White": 31365,
+   "Black": 353,
+   "American Indian and Alaska Native": 38,
+   "Asian and Pacific Islander": 34,
+   "Hispanic": 117
+ },
+ {
+   "County": "Butler",
+   "White": 243611,
+   "Black": 12193,
+   "American Indian and Alaska Native": 372,
+   "Asian and Pacific Islander": 1082,
+   "Hispanic": 1486
+ },
+ {
+   "County": "Carroll",
+   "White": 25301,
+   "Black": 89,
+   "American Indian and Alaska Native": 22,
+   "Asian and Pacific Islander": 58,
+   "Hispanic": 112
+ },
+ {
+   "County": "Champaign",
+   "White": 32350,
+   "Black": 955,
+   "American Indian and Alaska Native": 33,
+   "Asian and Pacific Islander": 65,
+   "Hispanic": 212
+ },
+ {
+   "County": "Clark",
+   "White": 135531,
+   "Black": 12964,
+   "American Indian and Alaska Native": 332,
+   "Asian and Pacific Islander": 404,
+   "Hispanic": 938
+ },
+ {
+   "County": "Clermont",
+   "White": 126584,
+   "Black": 818,
+   "American Indian and Alaska Native": 103,
+   "Asian and Pacific Islander": 257,
+   "Hispanic": 712
+ },
+ {
+   "County": "Clinton",
+   "White": 33541,
+   "Black": 712,
+   "American Indian and Alaska Native": 27,
+   "Asian and Pacific Islander": 50,
+   "Hispanic": 268
+ },
+ {
+   "County": "Columbiana",
+   "White": 111472,
+   "Black": 1345,
+   "American Indian and Alaska Native": 109,
+   "Asian and Pacific Islander": 215,
+   "Hispanic": 392
+ },
+ {
+   "County": "Coshocton",
+   "White": 35266,
+   "Black": 464,
+   "American Indian and Alaska Native": 44,
+   "Asian and Pacific Islander": 148,
+   "Hispanic": 98
+ },
+ {
+   "County": "Crawford",
+   "White": 49219,
+   "Black": 359,
+   "American Indian and Alaska Native": 101,
+   "Asian and Pacific Islander": 124,
+   "Hispanic": 255
+ },
+ {
+   "County": "Cuyahoga",
+   "White": 1120722,
+   "Black": 338254,
+   "American Indian and Alaska Native": 1799,
+   "Asian and Pacific Islander": 11879,
+   "Hispanic": 24393
+ },
+ {
+   "County": "Darke",
+   "White": 54497,
+   "Black": 119,
+   "American Indian and Alaska Native": 69,
+   "Asian and Pacific Islander": 94,
+   "Hispanic": 313
+ },
+ {
+   "County": "Defiance",
+   "White": 37234,
+   "Black": 181,
+   "American Indian and Alaska Native": 85,
+   "Asian and Pacific Islander": 103,
+   "Hispanic": 2372
+ },
+ {
+   "County": "Delaware",
+   "White": 52078,
+   "Black": 1211,
+   "American Indian and Alaska Native": 33,
+   "Asian and Pacific Islander": 147,
+   "Hispanic": 290
+ },
+ {
+   "County": "Erie",
+   "White": 72586,
+   "Black": 5775,
+   "American Indian and Alaska Native": 68,
+   "Asian and Pacific Islander": 157,
+   "Hispanic": 1061
+ },
+ {
+   "County": "Fairfield",
+   "White": 92676,
+   "Black": 316,
+   "American Indian and Alaska Native": 110,
+   "Asian and Pacific Islander": 273,
+   "Hispanic": 279
+ },
+ {
+   "County": "Fayette",
+   "White": 26387,
+   "Black": 710,
+   "American Indian and Alaska Native": 3,
+   "Asian and Pacific Islander": 84,
+   "Hispanic": 275
+ },
+ {
+   "County": "Franklin",
+   "White": 722340,
+   "Black": 129825,
+   "American Indian and Alaska Native": 1264,
+   "Asian and Pacific Islander": 7165,
+   "Hispanic": 7296
+ },
+ {
+   "County": "Fulton",
+   "White": 36080,
+   "Black": 29,
+   "American Indian and Alaska Native": 15,
+   "Asian and Pacific Islander": 60,
+   "Hispanic": 1547
+ },
+ {
+   "County": "Gallia",
+   "White": 28735,
+   "Black": 956,
+   "American Indian and Alaska Native": 98,
+   "Asian and Pacific Islander": 75,
+   "Hispanic": 207
+ },
+ {
+   "County": "Geauga",
+   "White": 72904,
+   "Black": 979,
+   "American Indian and Alaska Native": 17,
+   "Asian and Pacific Islander": 216,
+   "Hispanic": 313
+ },
+ {
+   "County": "Greene",
+   "White": 118660,
+   "Black": 8874,
+   "American Indian and Alaska Native": 203,
+   "Asian and Pacific Islander": 1133,
+   "Hispanic": 794
+ },
+ {
+   "County": "Guernsey",
+   "White": 41114,
+   "Black": 577,
+   "American Indian and Alaska Native": 55,
+   "Asian and Pacific Islander": 93,
+   "Hispanic": 142
+ },
+ {
+   "County": "Hamilton",
+   "White": 696126,
+   "Black": 164564,
+   "American Indian and Alaska Native": 1024,
+   "Asian and Pacific Islander": 5200,
+   "Hispanic": 5717
+ },
+ {
+   "County": "Hancock",
+   "White": 62400,
+   "Black": 396,
+   "American Indian and Alaska Native": 90,
+   "Asian and Pacific Islander": 220,
+   "Hispanic": 1446
+ },
+ {
+   "County": "Hardin",
+   "White": 32106,
+   "Black": 359,
+   "American Indian and Alaska Native": 26,
+   "Asian and Pacific Islander": 60,
+   "Hispanic": 156
+ },
+ {
+   "County": "Harrison",
+   "White": 17542,
+   "Black": 496,
+   "American Indian and Alaska Native": 18,
+   "Asian and Pacific Islander": 19,
+   "Hispanic": 75
+ },
+ {
+   "County": "Henry",
+   "White": 27086,
+   "Black": 87,
+   "American Indian and Alaska Native": 33,
+   "Asian and Pacific Islander": 36,
+   "Hispanic": 1138
+ },
+ {
+   "County": "Highland",
+   "White": 32556,
+   "Black": 723,
+   "American Indian and Alaska Native": 93,
+   "Asian and Pacific Islander": 30,
+   "Hispanic": 68
+ },
+ {
+   "County": "Hocking",
+   "White": 23918,
+   "Black": 152,
+   "American Indian and Alaska Native": 16,
+   "Asian and Pacific Islander": 21,
+   "Hispanic": 133
+ },
+ {
+   "County": "Holmes",
+   "White": 29194,
+   "Black": 8,
+   "American Indian and Alaska Native": 50,
+   "Asian and Pacific Islander": 8,
+   "Hispanic": 156
+ },
+ {
+   "County": "Huron",
+   "White": 53223,
+   "Black": 469,
+   "American Indian and Alaska Native": 80,
+   "Asian and Pacific Islander": 72,
+   "Hispanic": 764
+ },
+ {
+   "County": "Jackson",
+   "White": 30142,
+   "Black": 230,
+   "American Indian and Alaska Native": 25,
+   "Asian and Pacific Islander": 12,
+   "Hispanic": 165
+ },
+ {
+   "County": "Jefferson",
+   "White": 85617,
+   "Black": 5000,
+   "American Indian and Alaska Native": 124,
+   "Asian and Pacific Islander": 338,
+   "Hispanic": 387
+ },
+ {
+   "County": "Knox",
+   "White": 45778,
+   "Black": 240,
+   "American Indian and Alaska Native": 65,
+   "Asian and Pacific Islander": 78,
+   "Hispanic": 125
+ },
+ {
+   "County": "Lake",
+   "White": 207521,
+   "Black": 2814,
+   "American Indian and Alaska Native": 248,
+   "Asian and Pacific Islander": 1175,
+   "Hispanic": 1037
+ },
+ {
+   "County": "Lawrence",
+   "White": 61885,
+   "Black": 1445,
+   "American Indian and Alaska Native": 165,
+   "Asian and Pacific Islander": 65,
+   "Hispanic": 277
+ },
+ {
+   "County": "Licking",
+   "White": 117829,
+   "Black": 1985,
+   "American Indian and Alaska Native": 244,
+   "Asian and Pacific Islander": 303,
+   "Hispanic": 558
+ },
+ {
+   "County": "Logan",
+   "White": 38111,
+   "Black": 712,
+   "American Indian and Alaska Native": 47,
+   "Asian and Pacific Islander": 77,
+   "Hispanic": 202
+ },
+ {
+   "County": "Lorain",
+   "White": 240470,
+   "Black": 19263,
+   "American Indian and Alaska Native": 580,
+   "Asian and Pacific Islander": 1285,
+   "Hispanic": 13164
+ },
+ {
+   "County": "Lucas",
+   "White": 390991,
+   "Black": 63761,
+   "American Indian and Alaska Native": 1019,
+   "Asian and Pacific Islander": 2813,
+   "Hispanic": 12754
+ },
+ {
+   "County": "Madison",
+   "White": 31217,
+   "Black": 1501,
+   "American Indian and Alaska Native": 60,
+   "Asian and Pacific Islander": 87,
+   "Hispanic": 129
+ },
+ {
+   "County": "Mahoning",
+   "White": 242244,
+   "Black": 40635,
+   "American Indian and Alaska Native": 347,
+   "Asian and Pacific Islander": 744,
+   "Hispanic": 5363
+ },
+ {
+   "County": "Marion",
+   "White": 65023,
+   "Black": 2216,
+   "American Indian and Alaska Native": 95,
+   "Asian and Pacific Islander": 142,
+   "Hispanic": 461
+ },
+ {
+   "County": "Medina",
+   "White": 111156,
+   "Black": 850,
+   "American Indian and Alaska Native": 146,
+   "Asian and Pacific Islander": 308,
+   "Hispanic": 681
+ },
+ {
+   "County": "Meigs",
+   "White": 23222,
+   "Black": 225,
+   "American Indian and Alaska Native": 41,
+   "Asian and Pacific Islander": 12,
+   "Hispanic": 139
+ },
+ {
+   "County": "Mercer",
+   "White": 37862,
+   "Black": 14,
+   "American Indian and Alaska Native": 64,
+   "Asian and Pacific Islander": 50,
+   "Hispanic": 342
+ },
+ {
+   "County": "Miami",
+   "White": 87889,
+   "Black": 1782,
+   "American Indian and Alaska Native": 92,
+   "Asian and Pacific Islander": 217,
+   "Hispanic": 393
+ },
+ {
+   "County": "Monroe",
+   "White": 17342,
+   "Black": 5,
+   "American Indian and Alaska Native": 5,
+   "Asian and Pacific Islander": 6,
+   "Hispanic": 24
+ },
+ {
+   "County": "Montgomery",
+   "White": 469307,
+   "Black": 94138,
+   "American Indian and Alaska Native": 718,
+   "Asian and Pacific Islander": 3095,
+   "Hispanic": 3913
+ },
+ {
+   "County": "Morgan",
+   "White": 13581,
+   "Black": 514,
+   "American Indian and Alaska Native": 51,
+   "Asian and Pacific Islander": 20,
+   "Hispanic": 59
+ },
+ {
+   "County": "Morrow",
+   "White": 26227,
+   "Black": 33,
+   "American Indian and Alaska Native": 26,
+   "Asian and Pacific Islander": 69,
+   "Hispanic": 111
+ },
+ {
+   "County": "Muskingum",
+   "White": 79269,
+   "Black": 3637,
+   "American Indian and Alaska Native": 123,
+   "Asian and Pacific Islander": 109,
+   "Hispanic": 190
+ },
+ {
+   "County": "Noble",
+   "White": 11251,
+   "Black": 0,
+   "American Indian and Alaska Native": 29,
+   "Asian and Pacific Islander": 6,
+   "Hispanic": 24
+ },
+ {
+   "County": "Ottawa",
+   "White": 38596,
+   "Black": 211,
+   "American Indian and Alaska Native": 29,
+   "Asian and Pacific Islander": 33,
+   "Hispanic": 1207
+ },
+ {
+   "County": "Paulding",
+   "White": 20568,
+   "Black": 209,
+   "American Indian and Alaska Native": 16,
+   "Asian and Pacific Islander": 12,
+   "Hispanic": 488
+ },
+ {
+   "County": "Perry",
+   "White": 30767,
+   "Black": 60,
+   "American Indian and Alaska Native": 10,
+   "Asian and Pacific Islander": 20,
+   "Hispanic": 160
+ },
+ {
+   "County": "Pickaway",
+   "White": 42713,
+   "Black": 575,
+   "American Indian and Alaska Native": 55,
+   "Asian and Pacific Islander": 60,
+   "Hispanic": 231
+ },
+ {
+   "County": "Pike",
+   "White": 22245,
+   "Black": 240,
+   "American Indian and Alaska Native": 104,
+   "Asian and Pacific Islander": 62,
+   "Hispanic": 140
+ },
+ {
+   "County": "Portage",
+   "White": 130709,
+   "Black": 3450,
+   "American Indian and Alaska Native": 165,
+   "Asian and Pacific Islander": 814,
+   "Hispanic": 610
+ },
+ {
+   "County": "Preble",
+   "White": 37867,
+   "Black": 223,
+   "American Indian and Alaska Native": 37,
+   "Asian and Pacific Islander": 21,
+   "Hispanic": 68
+ },
+ {
+   "County": "Putnam",
+   "White": 31818,
+   "Black": 10,
+   "American Indian and Alaska Native": 2,
+   "Asian and Pacific Islander": 12,
+   "Hispanic": 1141
+ },
+ {
+   "County": "Richland",
+   "White": 120362,
+   "Black": 9187,
+   "American Indian and Alaska Native": 196,
+   "Asian and Pacific Islander": 334,
+   "Hispanic": 1046
+ },
+ {
+   "County": "Ross",
+   "White": 60987,
+   "Black": 3327,
+   "American Indian and Alaska Native": 102,
+   "Asian and Pacific Islander": 208,
+   "Hispanic": 351
+ },
+ {
+   "County": "Sandusky",
+   "White": 58599,
+   "Black": 1339,
+   "American Indian and Alaska Native": 77,
+   "Asian and Pacific Islander": 104,
+   "Hispanic": 3132
+ },
+ {
+   "County": "Scioto",
+   "White": 81367,
+   "Black": 2440,
+   "American Indian and Alaska Native": 190,
+   "Asian and Pacific Islander": 145,
+   "Hispanic": 386
+ },
+ {
+   "County": "Seneca",
+   "White": 59232,
+   "Black": 1143,
+   "American Indian and Alaska Native": 41,
+   "Asian and Pacific Islander": 99,
+   "Hispanic": 1334
+ },
+ {
+   "County": "Shelby",
+   "White": 42376,
+   "Black": 490,
+   "American Indian and Alaska Native": 2,
+   "Asian and Pacific Islander": 119,
+   "Hispanic": 98
+ },
+ {
+   "County": "Stark",
+   "White": 349386,
+   "Black": 24058,
+   "American Indian and Alaska Native": 602,
+   "Asian and Pacific Islander": 1126,
+   "Hispanic": 3414
+ },
+ {
+   "County": "Summit",
+   "White": 461681,
+   "Black": 56522,
+   "American Indian and Alaska Native": 598,
+   "Asian and Pacific Islander": 832,
+   "Hispanic": 2390
+ },
+ {
+   "County": "Trumbull",
+   "White": 224899,
+   "Black": 14585,
+   "American Indian and Alaska Native": 205,
+   "Asian and Pacific Islander": 846,
+   "Hispanic": 1216
+ },
+ {
+   "County": "Tuscarawas",
+   "White": 83225,
+   "Black": 750,
+   "American Indian and Alaska Native": 144,
+   "Asian and Pacific Islander": 126,
+   "Hispanic": 352
+ },
+ {
+   "County": "Union",
+   "White": 28728,
+   "Black": 548,
+   "American Indian and Alaska Native": 73,
+   "Asian and Pacific Islander": 84,
+   "Hispanic": 99
+ },
+ {
+   "County": "Van Wert",
+   "White": 29976,
+   "Black": 97,
+   "American Indian and Alaska Native": 14,
+   "Asian and Pacific Islander": 88,
+   "Hispanic": 283
+ },
+ {
+   "County": "Vinton",
+   "White": 11530,
+   "Black": 8,
+   "American Indian and Alaska Native": 11,
+   "Asian and Pacific Islander": 11,
+   "Hispanic": 22
+ },
+ {
+   "County": "Warren",
+   "White": 96839,
+   "Black": 1566,
+   "American Indian and Alaska Native": 169,
+   "Asian and Pacific Islander": 302,
+   "Hispanic": 359
+ },
+ {
+   "County": "Washington",
+   "White": 62905,
+   "Black": 892,
+   "American Indian and Alaska Native": 87,
+   "Asian and Pacific Islander": 172,
+   "Hispanic": 199
+ },
+ {
+   "County": "Wayne",
+   "White": 95310,
+   "Black": 1191,
+   "American Indian and Alaska Native": 34,
+   "Asian and Pacific Islander": 339,
+   "Hispanic": 497
+ },
+ {
+   "County": "Williams",
+   "White": 35414,
+   "Black": 6,
+   "American Indian and Alaska Native": 47,
+   "Asian and Pacific Islander": 107,
+   "Hispanic": 793
+ },
+ {
+   "County": "Wood",
+   "White": 102911,
+   "Black": 1316,
+   "American Indian and Alaska Native": 175,
+   "Asian and Pacific Islander": 536,
+   "Hispanic": 2355
+ },
+ {
+   "County": "Wyandot",
+   "White": 22426,
+   "Black": 18,
+   "American Indian and Alaska Native": 26,
+   "Asian and Pacific Islander": 32,
+   "Hispanic": 149
+ }
+]
+}
